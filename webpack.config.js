0ce8c4736b569458c070d448ee87abce42a3cc7d
Joey Palchak <joey.palchak@gmail.com>
2023-09-08 08:07:04 -0700

    Update project title.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
       verbose: true
     }),
     new HtmlWebpackPlugin({
-      title: 'Template',
+      title: 'Currency Exchange',
       template: './src/index.html',
       inject: 'body'
     }),

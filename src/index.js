047ff82cd331cce0d31359d0205ba0f1d7e68505
Joey Palchak <joey.palchak@gmail.com>
2023-09-08 12:02:07 -0700

    Add logic to call new service, storage, + dropdown.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './css/styles.css';
 import ExchangeService from './exchange-service.js';
 
 // TODO : Display more data
-// TODO : Implement currency choice for UI
 // TODO : Clean up Error handling
 // TODO : Separate backend logic
 // TODO+ : Implement cache of API results (if currency is called, save conversion rate)
 // TODO+ : Implement multiple conversions.
-// TODO+ : Implement dropdown selection for currencies.
 
 // BUSINESS LOGIC
 
@@ -24,9 +22,57 @@ export function getConversionRate(usd, query) {
       }
     });
 
+  console.log('GET CONVERSION');  // !!!!!!
+}
+
+export function getSupportedCodes() {
+  
+  ExchangeService.getSupportedCodes()
+    .then((currencies) => {
+      if (currencies.result === "success"){
+        addToSessionStorage(currencies["supported_codes"]);
+      } else {
+        displayError(currencies, "Supported Currencies");
+      }
+    });
+
+  console.log('GET CODES'); // !!!!!!
+}
+
+// for each currency, add it's code and corresponding country name to session storage
+export function addToSessionStorage(currencies) {
+
+  sessionStorage.clear(); // clear storage, just in case
+
+  currencies.forEach((currency) => {
+    sessionStorage.setItem(currency[0], currency[1]);
+  });
+
+  console.log("Added to storage."); // !!!!!!!!
 }
 
 // UI LOGIC
+
+// create dropdown selection for all available currencies
+// if session storage is empty, call ExchangeService API to GET supported currency codes and add to session storage
+function createSelectionForm() {
+  if (!sessionStorage.length) {
+    getSupportedCodes();
+  }
+
+  const selectForm = document.querySelector('select#target-currency');
+  const optionGroup = document.createElement('optgroup');
+  optionGroup.label = "Supported Currency";
+
+  Object.keys(sessionStorage).sort().forEach((code) => {
+    let option = document.createElement('option');
+    option.value = code;
+    option.innerText = sessionStorage.getItem(code);
+    optionGroup.append(option);
+  });
+  
+  selectForm.append(optionGroup);
+}
 
 // display conversion data in DOM for user specified currency query
 function displayConversion(response) {
@@ -40,6 +86,7 @@ function displayError(error, query) {
   document.querySelector("p#error-body").innerText = `${error.message}`;
 }
 
+// clear displayed results
 function clearResults() {
   document.querySelector("p#error-head").innerText = null;
   document.querySelector("p#error-body").innerText = null;
@@ -49,6 +96,9 @@ function clearResults() {
 // handle all UI Logic
 function handleEverything() {
 
+  // create dropdown selection for all available currencies
+  createSelectionForm();
+  
   // handle form submission
   document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
@@ -56,8 +106,8 @@ function handleEverything() {
     clearResults();
 
     const usdAmount = document.querySelector("input#usd-amount").value;
-    const targetCurrency = document.querySelector("input#target-currency").value;
-
+    // const targetCurrency = document.querySelector("input#target-currency").value;
+    const targetCurrency = document.querySelector("select#target-currency").value;
     getConversionRate(usdAmount, targetCurrency);
 
   });
